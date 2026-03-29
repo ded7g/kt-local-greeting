62e4a1c53611525222accfc8052e797c11dfc918
mc704ed <mykyta.churylov@student.tuke.sk>
2026-03-29 14:34:25 +0200

    ff

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -2,6 +2,7 @@ package mate.academy
 
 fun greetUsers() {
     while(true) {
+        println("Please enter a name (or press Enter to quit):")
         val string = readln()
         if (string == "") break
         println("Hello, $string!")
